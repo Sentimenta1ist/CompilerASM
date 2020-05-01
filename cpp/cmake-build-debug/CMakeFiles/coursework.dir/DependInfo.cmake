bd7cc67824f66ef856e7f4ae2b4715a21358d413
senti <sentimental1243@gmail.com>
2020-05-01 23:58:07 +0300

    add new lang

---
+++ cpp/cmake-build-debug/CMakeFiles/coursework.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/sentimental/Projects/CLionProjects/CourseWorkASM/Addition.cpp" "/home/sentimental/Projects/CLionProjects/CourseWorkASM/cmake-build-debug/CMakeFiles/coursework.dir/Addition.cpp.o"
+  "/home/sentimental/Projects/CLionProjects/CourseWorkASM/Base.cpp" "/home/sentimental/Projects/CLionProjects/CourseWorkASM/cmake-build-debug/CMakeFiles/coursework.dir/Base.cpp.o"
+  "/home/sentimental/Projects/CLionProjects/CourseWorkASM/ErrorCheck.cpp" "/home/sentimental/Projects/CLionProjects/CourseWorkASM/cmake-build-debug/CMakeFiles/coursework.dir/ErrorCheck.cpp.o"
+  "/home/sentimental/Projects/CLionProjects/CourseWorkASM/FirstSecondStage.cpp" "/home/sentimental/Projects/CLionProjects/CourseWorkASM/cmake-build-debug/CMakeFiles/coursework.dir/FirstSecondStage.cpp.o"
+  "/home/sentimental/Projects/CLionProjects/CourseWorkASM/MachineCode.cpp" "/home/sentimental/Projects/CLionProjects/CourseWorkASM/cmake-build-debug/CMakeFiles/coursework.dir/MachineCode.cpp.o"
+  "/home/sentimental/Projects/CLionProjects/CourseWorkASM/Parsing.cpp" "/home/sentimental/Projects/CLionProjects/CourseWorkASM/cmake-build-debug/CMakeFiles/coursework.dir/Parsing.cpp.o"
+  "/home/sentimental/Projects/CLionProjects/CourseWorkASM/main.cpp" "/home/sentimental/Projects/CLionProjects/CourseWorkASM/cmake-build-debug/CMakeFiles/coursework.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
