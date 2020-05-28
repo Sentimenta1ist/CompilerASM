2bd78899d336a810603542cb6e9aa15ac3dda8fb
sentimentGod <sentimental1243@gmail.com>
2020-05-29 00:00:01 +0300

    commit

--- python/main.py
+++ python/main.py
@@ -28,5 +28,5 @@ if __name__ == "__main__":
         base_information.all_lines_of_lexemes.append(OneLine(line))
 
     for line in base_information.all_lines_of_lexemes:
-        print(line.displacement)
+        print(line.displacement + '  ')
         print(line)
