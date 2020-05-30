1e29574cebe0db1d0e1c88752bd8f589dd36833f
sentimentGod <sentimental1243@gmail.com>
2020-05-30 23:48:09 +0300

    add dictionary of instruction

--- python/base_information.py
+++ python/base_information.py
@@ -1,19 +1,87 @@
 REG_8 = 'reg 8'
 REG_16 = 'reg 16'
+
+MEM_8 = 'mem 8'
+MEM_16 = 'mem 16'
+
+IMM_8 = 'imm 8'
+IMM_16 = 'imm 16'
+
+SEG_REG = 'segment register'
+
 DIRECTIVE = 'directive'
 INSTRUCTION = 'instruction'
 SEGMENT_WORD = 'segment word'
 LABEL_SEGMENT = 'segment label'
 ONE_SYMBL_LEXEME = 'one smb lexeme'
+NONE_OPERAND = 'no operand'
+
+
+# 48+rw     DEC r16
+# FE /1     DEC r/m8
+
+# AD        LODS m16
+# AC        LODS m8
+
+# 84 /r     TEST r/m8, r8
+# 85 /r     TEST r/m16, r16
+
+# 08 /r     OR r/m8, r8
+# 09 /r     OR r/m16, r16
+# 0A /r     OR r8, r/m8
+# 0B /r     OR r16, r/m16
+#
+# C0 /0 ib  ROL r/m8, imm8
+# C0 /0 ib  ROL r/m16, imm8
+#
+# 2F        DAS
+#
+# 80 /0 ib  ADD r/m8, imm8
+# 81 /0 iw  ADD r/m16, imm16
+# 83 /0 ib  ADD r/m16, imm8
+#
+# 04 ib     ADD AL, imm8
+# 05 iw     ADD AX, imm16
+#
+# 88 /r     MOV r/m8, r8
+# 89 /r     MOV r/m16, r16
+# 8C /r     MOV r/m16, S_reg
 
 
 class InstructionInformation(object):
-    def __init__(self, name, opcode):
-        self.name = name;
+    def __init__(self, name, op_code, operand_1, operand_2, mod_rm, reg_in_op_code):
+        self.name = name
+        self.op_code = op_code
+        self.operand_1 = operand_1
+        self.operand_2 = operand_2
+        self.mod_rm = mod_rm
+        self.reg_in_op_code = reg_in_op_code
 
 
+dictionary_of_instructions = [
+    InstructionInformation("dec", 0x48, REG_8, NONE_OPERAND, mod_rm=0, reg_in_op_code=1),
+    InstructionInformation("dec", 0xFE, REG_16, NONE_OPERAND, mod_rm=1, reg_in_op_code=0),
 
-        
+    InstructionInformation("lods", 0xAD, MEM_8, NONE_OPERAND, mod_rm=0, reg_in_op_code=0),
+    InstructionInformation("lods", 0xAC, MEM_8, NONE_OPERAND, mod_rm=0, reg_in_op_code=0),
+
+    InstructionInformation("test", 0x84, REG_8, REG_8, mod_rm=1, reg_in_op_code=0),
+    InstructionInformation("test", 0x85, REG_16, REG_16, mod_rm=1, reg_in_op_code=0),
+
+    InstructionInformation("or", 0x0A, REG_16, MEM_16, mod_rm=1, reg_in_op_code=0),
+    InstructionInformation("or", 0x0A, REG_8, MEM_8, mod_rm=1, reg_in_op_code=0),
+
+    InstructionInformation("das", 0x2F, NONE_OPERAND, NONE_OPERAND, mod_rm=0, reg_in_op_code=0),
+
+    InstructionInformation("add", 0x80, REG_8, IMM_8, mod_rm=0, reg_in_op_code=0),
+    InstructionInformation("add", 0x81, REG_8, IMM_8, mod_rm=0, reg_in_op_code=0),
+    InstructionInformation("add", 0x83, REG_16, IMM_8, mod_rm=0, reg_in_op_code=0),
+
+    InstructionInformation("mov", 0x88, MEM_8, REG_8, mod_rm=1, reg_in_op_code=0),
+    InstructionInformation("mov", 0x89, MEM_16, REG_16, mod_rm=1, reg_in_op_code=0),
+    InstructionInformation("mov", 0x8C, MEM_8, SEG_REG, mod_rm=1, reg_in_op_code=0)
+
+]
 
 types_of_lexemes = {
     REG_16: ['ax', 'cx', 'dx', 'bx', 'sp', 'bp', 'si', 'di'],
--- python/main.py
+++ python/main.py
@@ -6,12 +6,32 @@ from line import OneLine
 from line import GlobalDisplacement
 
 
+def segment_table():
+    print("\n|   |Name           |Bit        |Size       |\n"
+          "|---|---------------|-----------|-----------|\n"
+          "| 0 |DATA           |16         |000B       |\n"
+          "| 1 |CODE           |16         |0019       |")
+    print("\n\n|#  |Segment            |Exp           |\n"
+          "|---|-------------------|--------------|\n"
+          "|0  |DS                 |DATA          |\n"
+          "|1  |CS                 |CODE          |\n"
+          "|2  |SS                 |NOTHING       |\n"
+          "|3  |ES                 |NOTHING       |\n"
+          "|4  |GS                 |NOTHING       |\n"
+          "|5  |FS                 |NOTHING       |\n")
+
+def table_of_user():
+    print("| # |Name       |Type       |Value    |Attr      |\n"
+          "|---|-----------|-----------|---------|----------|")
+
+
+
 def lexeme_and_syntax(file_name):
     file = open(file_name, "r")
     lines_of_file = file.read().splitlines()
     i = 0
     for line_of_lex in base_information.all_lexemes:
-        print(lines_of_file[i]+'\n')
+        print(lines_of_file[i] + '\n')
         i += 1
         print("| # |Lex            |     Len|Type               |\n"
               "|---|---------------|--------|-------------------|")
@@ -21,6 +41,7 @@ def lexeme_and_syntax(file_name):
 
 
 if __name__ == "__main__":
+
     read_from_file("test.asm")
     lexeme_and_syntax("test.asm")
 
@@ -29,3 +50,6 @@ if __name__ == "__main__":
 
     for line in base_information.all_lines_of_lexemes:
         print('{}               {}'.format(line.displacement, line))
+
+    segment_table()
+    table_of_user()
